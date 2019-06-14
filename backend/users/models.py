769433263c84e9ed5b832bc710776020671b2d3e
wojwarych <woj.warych@gmail.com>
2019-06-14 16:16:17 +0200

    added is_student field

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,5 +4,6 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     is_trainer = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=True)
     phone_number = models.CharField(max_length=9)
     birth_date = models.DateField(null=True, blank=True)
