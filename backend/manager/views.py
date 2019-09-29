a7b7aa00318302d535c280245fdf356380656f73
wojwarych <woj.warych@gmail.com>
2019-09-29 19:34:57 +0200

    added basics of views for manager

--- backend/manager/views.py
+++ backend/manager/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.models import Group
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
+from users.models import User
 from utils.extra_context import ExtraContext
 
 
@@ -10,3 +12,24 @@ class ManagerView(LoginRequiredMixin, ExtraContext, TemplateView):
     login_url = "/signin/"
     redirect_field_name = "redirect_to"
     template_name = "manager/manager.html"
+
+
+class StudentsList(LoginRequiredMixin, ExtraContext, ListView):
+    model = User
+    paginate_by = 20
+    template_name = "manager/user_list.html"
+    queryset = User.objects.exclude(is_trainer=True)
+
+
+class TrainersList(LoginRequiredMixin, ExtraContext, ListView):
+    model = User
+    paginate_by = 20
+    template_name = "manager/user_list.html"
+    queryset = User.objects.exclude(is_trainer=False)
+
+
+class GroupsList(LoginRequiredMixin, ExtraContext, ListView):
+    model = Group
+    paginate_by = 10
+    template_name = "manager/groups.html"
+    queryset = Group.objects.all()
