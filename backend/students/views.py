cc70f7e6b1ca1d1979d2992a8da56438872efc47
wojwarych <woj.warych@gmail.com>
2019-09-29 20:04:15 +0200

    moved signin signout from students to users app

--- backend/students/views.py
+++ backend/students/views.py
@@ -1,8 +1,5 @@
-from django.contrib.auth import login as auth_login, logout as auth_logout
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
-from django.views.generic import FormView, RedirectView
 from django.views.generic.base import TemplateView
 
 from utils.extra_context import ExtraContext
@@ -46,25 +43,3 @@ class PersonalDetail(LoginRequiredMixin, ExtraContext, TemplateView):
     login_url = "/signin/"
     redirect_field_name = "redirect_to"
     template_name = "students/personal_detail.html"
-
-
-class SignInView(ExtraContext, FormView):
-    login_url = "/signin/"
-    redirect_field_name = "redirect_to"
-    template_name = "students/signin_form.html"
-    form_class = AuthenticationForm
-
-    def form_valid(self, form):
-        auth_login(self.request, form.get_user())
-        self.success_url = ("/manager/"
-                            if form.get_user().is_staff
-                            else "/home/")
-        return super(SignInView, self).form_valid(form)
-
-
-class SignOutView(ExtraContext, RedirectView):
-    url = "/signin/"
-
-    def get(self, request, *args, **kwargs):
-        auth_logout(request)
-        return super(SignOutView, self).get(request, *args, **kwargs)
