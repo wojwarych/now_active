a9fd1850cd4400a485e98cbcd030dfe824b1962f
wojwarych <woj.warych@gmail.com>
2020-03-28 23:08:52 +0100

    Task 28 - basic models for app designed

--- backend/manager/models.py
+++ backend/manager/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+from users.models import Trainer, User
+
+PLAYERS_LEVELS = (("B", "Beginner"), ("I", "Intermediate"), ("A", "Advanced"))
+
+
+class PlayersClass(models.Model):
+    level = models.CharField(max_length=1, choices=PLAYERS_LEVELS)
+    players = models.ManyToManyField(User, related_name="class_students")
+    training_place = models.OneToOneField(
+        "TrainingPlace", on_delete=models.CASCADE
+    )
+    trainer = models.ForeignKey(
+        Trainer, on_delete=models.CASCADE, related_name="trainer_classes"
+    )
+    training_date = models.DateTimeField()
+
+
+def __str__(self):
+    return f'Class {self.level} at {self.training_place} with {self.trainer}'
+
+
+class TrainingPlace(models.Model):
+    name = models.CharField(max_length=64, blank=False)
+    city = models.CharField(max_length=64, blank=False)
+    address = models.CharField(max_length=64, null=False, blank=False)
+    open_from = models.DateTimeField()
+    closed_at = models.DateTimeField()
+    trainer = models.ManyToManyField(
+        Trainer, related_name="trainings_trainers"
+    )
+
+    @property
+    def full_address(self):
+        return f"{self.city} {self.address}"
+
+    @property
+    def working_hours(self):
+        return (self.open_from, self.closed_at)
+
+    def __str__(self):
+        return f"{self.name} in {self.city} {self.address}"
--- backend/students/models.py
+++ backend/students/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_payments'
+    )
+    value = models.DecimalField(max_digits=4, decimal_places=2)
+    deadline = models.DateField()
+
+    constraints = models.UniqueConstraint(
+        fields=['user', 'deadline'], name='user_deadline_constraint'
+    )
+
+    def __str__(self):
+        return f'User {self.user} payment of {self.value} to {self.deadline}'
